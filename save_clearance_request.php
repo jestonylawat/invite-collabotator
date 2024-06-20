5263975c807b441128b66c37c2e12a5185a404ce
Jestoni Amboang <162429573+Amcrazyyy@users.noreply.github.com>
2024-06-20 16:18:14 +0300

    Update save_clearance_request.php

--- save_clearance_request.php
+++ save_clearance_request.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1); 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname); 
 
 // Check connection
 if ($conn->connect_error) {
