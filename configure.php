30eaf0446d8d71afffa25251e0a43bf85528bc46
jestonylawat <164851897+jestonylawat@users.noreply.github.com>
2024-06-20 16:27:53 +0300

    Update configure.php

--- configure.php
+++ configure.php
@@ -4,7 +4,7 @@ $username = 'root';
 $password = '';
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
 ];
 
 try {
